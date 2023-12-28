43e81bb844fbd2877adbb0e4c26dafe096621ba0
정은아 <jeong-eun-a@jeong-eun-aui-iMac.local>
2023-12-28 15:59:47 +0900

    test

---
+++ 231228/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  231228
+//
+//  Created by 정은아 on 2023/12/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
